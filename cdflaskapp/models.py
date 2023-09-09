ae8c52fca791a58bd94c43d990518df1a129bf98
Lavi Ionas <laviionas772@gmail.com>
2023-09-09 17:08:00 -0400

    create Event model

--- cdflaskapp/models.py
+++ cdflaskapp/models.py
@@ -1,5 +1,22 @@
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 from . import db
+
+class Events(db.Model):
+    event_id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(20))
+    event_time = db.Column(DateTime(timezone=True))
+    event_duration = db.Column(db.Float)
+    event_status = db.Column(db.Boolean, default=True)
+
+    def __init__(self, name, time, duration, status):
+        self.event_name = name
+        self.event_time = time
+        self.event_duration = duration
+        self.event_status = status
+
+    def __repr__(self):
+        return '<Event {}>'.format(self.event_name)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)

913e452be8a3d8b731f103e8997128b379f8497a
Lavi Ionas <laviionas772@gmail.com>
2023-09-09 17:59:35 -0400

    set up events routes

--- cdflaskapp/main.py
+++ cdflaskapp/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from .models import User, Event
 from . import db
 
 main = Blueprint('main', __name__)
@@ -11,4 +12,23 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    events = Event.query.all()
+    return render_template('profile.html',
+                            name=current_user.name,
+                            events=events)
+
+@main.route('/add_event', methods=['POST'])
+@login_required
+def add_event():
+    name = request.form.get('event_name')
+    # time = request.form.get('event_time')
+    # duration = request.form.get('event_duration')
+    # status = request.form.get('event_status')
+
+    new_event = Event(user_id=current_user.id,
+                      name=name)
+
+    db.session.add(new_event)
+    db.session.commit()
+
+    return redirect('/profile')
--- cdflaskapp/models.py
+++ cdflaskapp/models.py
@@ -2,18 +2,20 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 from . import db
 
-class Events(db.Model):
+class Event(db.Model):
     event_id = db.Column(db.Integer, primary_key=True)
+    event_user_id = db.Column(db.Integer)
     event_name = db.Column(db.String(20))
-    event_time = db.Column(DateTime(timezone=True))
-    event_duration = db.Column(db.Float)
-    event_status = db.Column(db.Boolean, default=True)
+    # event_time = db.Column(DateTime(timezone=True))
+    # event_duration = db.Column(db.Float)
+    # event_status = db.Column(db.Boolean, default=True)
 
-    def __init__(self, name, time, duration, status):
+    def __init__(self, user_id, name):
+        self.event_user_id = user_id
         self.event_name = name
-        self.event_time = time
-        self.event_duration = duration
-        self.event_status = status
+        # self.event_time = time
+        # self.event_duration = duration
+        # self.event_status = status
 
     def __repr__(self):
         return '<Event {}>'.format(self.event_name)
